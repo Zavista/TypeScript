83fdb4d99960223e6c9e59854e79f4ee8f48e34d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-05-02 16:26:17 -0400

    Set up better workflow

--- dist/01TypeBasics.js
+++ dist/01TypeBasics.js
@@ -1,16 +1,14 @@
-let age = 30;
+"use strict";
+let age = 22;
 //TypeScript can "infer" the type of a variable
-
 // age = 'hello'
 // Types are static and cannot be changed
-
-let five: number = 5;
+let five = 5;
 // This is an explicit type annotation
-
-let firstName: string = "David";
-
-let isTrue: boolean = true;
-
-let something: null = null;
-let anotherThing: undefined = undefined;
+let firstName = "David";
+let isTrue = true;
+let something = null;
+let anotherThing = undefined;
 // This is bad because these are permanently only null/undefined
+console.log(age);
+clearImmediate;
---
+++ src/01TypeBasics.ts
@@ -0,0 +1,19 @@
+let age = 21;
+//TypeScript can "infer" the type of a variable
+
+// age = 'hello'
+// Types are static and cannot be changed
+
+let five: number = 5;
+// This is an explicit type annotation
+
+let firstName: string = "David";
+
+let isTrue: boolean = true;
+
+let something: null = null;
+let anotherThing: undefined = undefined;
+// This is bad because these are permanently only null/undefined
+
+console.log(age);
+clearImmediate;
